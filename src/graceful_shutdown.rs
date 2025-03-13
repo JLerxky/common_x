847c879b3fb8b86e4161ba4064588067cba4080a
JLer <jlerxky@live.com>
2025-03-13 18:14:36 +0800

    bump 0.8.4

--- src/graceful_shutdown.rs
+++ src/graceful_shutdown.rs
@@ -1,21 +1,21 @@
 use std::{
-    sync::{Mutex, OnceLock},
+    sync::{Arc, Mutex, OnceLock},
     time::Duration,
 };
 
 use flume::{Receiver, Sender};
 
-pub fn close_chain() -> &'static Mutex<CloseChain> {
-    static CLOSE_CHAIN: OnceLock<Mutex<CloseChain>> = OnceLock::new();
-    CLOSE_CHAIN.get_or_init(|| Mutex::new(CloseChain::init()))
+pub fn close_chain() -> &'static Arc<CloseChain> {
+    static CLOSE_CHAIN: OnceLock<Arc<CloseChain>> = OnceLock::new();
+    CLOSE_CHAIN.get_or_init(|| Arc::new(CloseChain::default()))
 }
 
-pub struct CloseHandler {
+pub struct CloseToken {
     close_rv: Receiver<()>,
     _prev_tx: Option<Sender<()>>,
 }
 
-impl CloseHandler {
+impl CloseToken {
     pub fn handle(&self) {
         self.close_rv.recv().ok();
     }
@@ -32,35 +32,32 @@ impl CloseHandler {
 }
 
 #[derive(Default)]
-pub struct CloseChain(Vec<(Sender<()>, Receiver<()>)>);
+pub struct CloseChain(Mutex<Vec<(Sender<()>, Receiver<()>)>>);
 
 impl CloseChain {
-    pub fn init() -> Self {
-        Self(Vec::new())
+    pub fn close(&self) {
+        self.0.lock().unwrap().clear();
     }
 
-    pub fn close(&mut self) {
-        self.0.clear();
-    }
-
-    pub fn handler(&mut self, deep: usize) -> CloseHandler {
-        let len = self.0.len();
+    pub fn token(&self, deep: usize) -> CloseToken {
+        let mut chain = self.0.lock().unwrap();
+        let len = chain.len();
 
         if deep >= len {
             for _ in len..=deep {
-                self.0.push(flume::bounded(0));
+                chain.push(flume::bounded(0));
             }
         }
 
         if deep == 0 {
-            CloseHandler {
-                close_rv: self.0[deep].1.clone(),
+            CloseToken {
+                close_rv: chain[deep].1.clone(),
                 _prev_tx: None,
             }
         } else {
-            CloseHandler {
-                close_rv: self.0[deep].1.clone(),
-                _prev_tx: Some(self.0[deep - 1].0.clone()),
+            CloseToken {
+                close_rv: chain[deep].1.clone(),
+                _prev_tx: Some(chain[deep - 1].0.clone()),
             }
         }
     }
